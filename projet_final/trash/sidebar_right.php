bff1cb8547d11b3117e9322c3f5c61691d42cd5a
Joris Brandt <jorisbrandt@gmail.com>
2016-10-19 01:25:12 +0200

    final

--- projet_final/trash/sidebar_right.php
+++
@@ -1,8 +0,0 @@
-<section>
-  <h2>Edito</h2>
-  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-</section>
-<section>
-  <h2>A propos de</h2>
-  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-</section>
